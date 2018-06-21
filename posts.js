6757da759125237e7b8d463215d28d26fa48951c
theflorencefu <florencefu2018@u.northwestern.edu>
2018-06-21 14:43:44 -0500

    align images to grid

--- posts.js
+++ posts.js
@@ -31,21 +31,31 @@ function textifyImages() {
     var widthUnits = Math.floor(Math.random() * 10 + 10);
 
     imageContainer.style.width = widthUnits * SLASH_WIDTH;
-    imageContainer.style.left = document.body.clientWidth/2 + Math.floor(Math.random() * 100);
+
+    var offsetLeft = document.body.clientWidth/2 + Math.floor(Math.random() * 100);
+    offsetLeft -= offsetLeft % SLASH_WIDTH;
+    imageContainer.style.left = offsetLeft;
 
     var imageRect = imageContainer.querySelector('.projects-post-img');
+    var offsetTop = imageRect.top - imageRect.top % SLASH_HEIGHT;
+    imageContainer.style.top = offsetTop;
 
     var heightUnits = Math.floor(imageRect.height/SLASH_HEIGHT);
 
     var p = document.createElement('p');
     var text = '';
     for (var j = 0; j < widthUnits * heightUnits; j++) {
-      text += FORWARD_SLASH;
+      text += i % 2 === 0 ? FORWARD_SLASH : BACK_SLASH;
     }
+
     p.innerHTML = text;
-    p.style.transform = 'translateY(-100%)';
+    p.style.position = 'absolute';
+    p.style.top = 0;
+    p.style.left = 0;
+    p.style.width = '100%';
     p.style.lineHeight = SLASH_HEIGHT + 'px';
     p.style.margin = 0;
+    p.style.letterSpacing = 0;
 
     imageContainer.appendChild(p);
   }
