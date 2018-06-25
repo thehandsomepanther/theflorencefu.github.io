4b781080cd89a0130cfdba11ce8f4ec8b09ff8e2
Josh Shi <thehandsomepanther@gmail.com>
2018-06-25 16:19:19 -0500

    improve image sizes

--- posts.js
+++ posts.js
@@ -1,61 +1,68 @@
-var FORWARD_SLASH = '╱';
-var BACK_SLASH = '╲';
+var FORWARD_SLASH = "╱";
+var BACK_SLASH = "╲";
 
-var dummySlashEl = document.createElement('span');
+var dummySlashEl = document.createElement("span");
 dummySlashEl.innerHTML = FORWARD_SLASH;
-document.querySelector('body').appendChild(dummySlashEl);
+document.querySelector("body").appendChild(dummySlashEl);
 
 var rect = dummySlashEl.getBoundingClientRect();
-console.log(rect);
 dummySlashEl.remove();
 var SLASH_WIDTH = rect.width;
 var SLASH_HEIGHT = rect.height;
 
 var loadedImages = 0;
-var postImageContainers = document.querySelectorAll('.projects-post-img-container');
+var postImageContainers = document.querySelectorAll(
+  ".projects-post-img-container"
+);
 for (var i = 0; i < postImageContainers.length; i++) {
-  var postImage = postImageContainers[i].querySelector('.projects-post-img');
-  postImage.addEventListener('load', function() {
+  var postImage = postImageContainers[i].querySelector(".projects-post-img");
+  postImage.addEventListener("load", function() {
     loadedImages++;
 
     if (loadedImages === postImageContainers.length) {
-      textifyImages()
+      textifyImages();
     }
   });
 }
 
 function textifyImages() {
+  var MAX_WIDTH_UNITS = document.body.clientWidth / (2 * SLASH_WIDTH);
+
   for (var i = 0; i < postImageContainers.length; i++) {
     var imageContainer = postImageContainers[i];
-
-    var widthUnits = Math.floor(Math.random() * 10 + 10);
+    var widthUnits = MAX_WIDTH_UNITS / Math.floor(Math.random() * 3 + 2);
 
     imageContainer.style.width = widthUnits * SLASH_WIDTH;
 
-    var offsetLeft = document.body.clientWidth/2 + Math.floor(Math.random() * 100);
+    var offsetLeft =
+      document.body.clientWidth / 3 +
+      Math.random() * (MAX_WIDTH_UNITS - widthUnits) * SLASH_WIDTH;
     offsetLeft -= offsetLeft % SLASH_WIDTH;
-    imageContainer.style.left = offsetLeft;
+    imageContainer.style.left = offsetLeft + "px";
 
-    var imageRect = imageContainer.querySelector('.projects-post-img');
-    var offsetTop = imageRect.top - imageRect.top % SLASH_HEIGHT;
-    imageContainer.style.top = offsetTop;
+    var imageRect = imageContainer
+      .querySelector(".projects-post-img")
+      .getBoundingClientRect();
+    var offsetTop = imageRect.top - (imageRect.top % SLASH_HEIGHT);
+    imageContainer.style.top = offsetTop + "px";
 
-    var heightUnits = Math.floor(imageRect.height/SLASH_HEIGHT);
+    var heightUnits = Math.floor(imageRect.height / SLASH_HEIGHT);
 
-    var p = document.createElement('p');
-    var text = '';
-    for (var j = 0; j < widthUnits * heightUnits; j++) {
+    var p = document.createElement("p");
+    var text = "";
+    for (var j = 0; j < Math.floor(widthUnits) * Math.floor(heightUnits); j++) {
       text += i % 2 === 0 ? FORWARD_SLASH : BACK_SLASH;
     }
 
     p.innerHTML = text;
-    p.style.position = 'absolute';
+    p.style.position = "absolute";
     p.style.top = 0;
     p.style.left = 0;
-    p.style.width = '100%';
-    p.style.lineHeight = SLASH_HEIGHT + 'px';
+    p.style.width = "100%";
+    p.style.lineHeight = SLASH_HEIGHT + "px";
     p.style.margin = 0;
     p.style.letterSpacing = 0;
+    p.style.fontFamily = "monospace";
 
     imageContainer.appendChild(p);
   }
